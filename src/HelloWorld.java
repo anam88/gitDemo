08a6b4d9636d71be6601ee807baa175007def3aa
anam88 <47282027+anam88@users.noreply.github.com>
2020-08-01 15:07:04 -0500

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello! Today was a Git class with Asiya.");
         System.out.println("Today is August 1 2020, doing my first commit to git!");
+        System.out.println("git pull command");
     }
 
 }
