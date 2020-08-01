c92ee118dfcd73cb74c3d0b13f3f45f3a2d90ad7
anam88 <amasroor88@gmail.com>
2020-08-01 14:59:57 -0500

    add more comments

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,6 +1,9 @@
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+
 public class BookARoom {
     public static void main(String[] args) {
 
         System.out.println("This is a git class");
+        System.out.println("Today is a Aug. 1st 2020. \n\tSecond day of git class.");
     }
 }
