6d325adc87c4319c17dc5e63a23b337c3111bf9c
anam88 <amasroor88@gmail.com>
2020-08-01 11:47:44 -0500

    Day2BiggerNumberTask

--- src/BeforeLunchTask.java
+++ src/BeforeLunchTask.java
@@ -6,5 +6,23 @@ public class BeforeLunchTask {
         int sum = num1 + num2 + num3;
 
         System.out.println("Sum of numbers: " +sum);
+        System.out.println("==================================");
+        //write a program that check which is bigger number
+
+
+        boolean n1 = num1 > num2 && num1 > num3;
+        boolean n2 = num2 > num1 && num2 > num3;
+        boolean n3 = num3 > num1 && num3 > num2;
+
+        if(n1){
+            System.out.println("n1 is bigger");
+        }
+        if(n2){
+            System.out.println("n2 is bigger");
+        }
+        if(n3){
+            System.out.println("n3 is bigger");
+        }
+
     }
 }
