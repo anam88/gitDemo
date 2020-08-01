188dfc8fe6357b24d225cdc364e84579449f6da1
anam88 <47282027+anam88@users.noreply.github.com>
2020-08-01 15:33:35 -0500

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -3,6 +3,7 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("Today is the 2nd day of Git class with Asiya" );
         System.out.println("Today the weather is sunny!");
+        System.out.println("git fetch and git merge");
 
     }
 }
